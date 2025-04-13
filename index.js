18102d0bcd8e42f5fb5827786ff9d4ba523b9a29
Victor Oliveira Melo <vmelo8044@gmail.com>
2025-04-12 21:14:35 -0400

    Calculadora

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Happy developing ✨')
+(function() {
+    let tela = document.querySelector('.tela');
+    let botoes = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let igual = document.querySelector('.btn-igual');
+
+    //Estutura de repetição
+    botoes.forEach(function(botoes) {
+        botoes.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            //Primeira estrutura de tomada de decisão
+            if (value !== undefined) {
+                tela.value += value;
+            }
+        });
+    });
+
+    igual.addEventListener('click', function(e) {
+        //Segunda estrutura de tomada de decisão
+        if (tela.value === '') {
+            tela.value = "Please enter";
+        } else {
+            try {
+                let answer = eval(tela.value);
+                tela.value = answer;
+            } catch (error) {
+                tela.value = "Error";
+            }
+        }
+    });
+
+    clear.addEventListener('click', function(e) {
+        tela.value = "";
+    });
+})()
